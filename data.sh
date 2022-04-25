32955839356b18fbf2fc64ee684c40dfe69ab6c6
Isabella Xue <71396377+isabellaxue@users.noreply.github.com>
2022-04-24 21:43:48 -0500

    change to chtc version

--- data.sh
+++ data.sh
@@ -1,8 +1,19 @@
 #!/bin/bash
 
-module load R/R-4.0.1 # tell execute computer where to find R software
+# untar your R and jsonlite installation
+tar -xzf R402.tar.gz
+tar -xzf packages_jsonlite.tar.gz
 
-RpackagesDir="R/library" # the R script will install packages here
-mkdir --parents "$RpackagesDir" # make directory & parents; no error if exists
+# make sure the script will use your R installation,
+# and the working directory as its home location
+export PATH=$PWD/R/bin:$PATH
+export RHOME=$PWD/R
+export R_LIBS=$PWD/packages
 
-Rscript data.R "$RpackagesDir"
+#create log, error, output directory
+#mkdir -p log error output
+
+# run your script
+Rscript data.R $1
+# note: the two actual command-line arguments are in data.sub's "arguments = " line
+
