77b5ad6f5644a3de8791b89f02369204c0062fa0
Isabella Xue <71396377+isabellaxue@users.noreply.github.com>
2022-04-24 21:44:37 -0500

    change to splitting 70 json files

--- getData.sh
+++ getData.sh
@@ -4,4 +4,4 @@ wget http://snap.stanford.edu/data/amazon/productGraph/user_dedup.json.gz
 
 gzip -d user_dedup.json.gz
 
-awk 'NR%2789333==1{x="J"++i".json";}{print > x}' user_dedup.json
+awk 'NR%1195429==1{x="J"++i".json";}{print > x}' user_dedup.json
