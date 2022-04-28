c651db1bfc4ec1ba6f9354999d067cddc7c0a5f8
Isabella Xue <71396377+isabellaxue@users.noreply.github.com>
2022-04-28 15:11:58 -0500

    Ready for parallel jobs

--- getData.sh
+++ getData.sh
@@ -4,4 +4,4 @@ wget http://snap.stanford.edu/data/amazon/productGraph/user_dedup.json.gz
 
 gzip -d user_dedup.json.gz
 
-awk 'NR%1195429==1{x="J"++i".json";}{print > x}' user_dedup.json
+awk 'NR%10000==1{x="J"++i".json";}{print > x}' user_dedup.json
