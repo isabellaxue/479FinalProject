b6f006a99df881194939a028087e284c4d59b789
Isabella Xue <71396377+isabellaxue@users.noreply.github.com>
2022-04-29 23:15:03 -0500

    Update question.sh

--- question.sh
+++ question.sh
@@ -1,27 +1,16 @@
 #!/bin/bash
 tar -xzf R402.tar.gz
-#use package wordcloud2, 
-tar -xzf packages_question.tar.gz
+tar -xzf packages_all.tar.gz
 
+# make sure the script will use your R installation,
+# and the working directory as its home location
 export PATH=$PWD/R/bin:$PATH
 export RHOME=$PWD/R
 export R_LIBS=$PWD/packages
 
-#Question 1
-cat bing_*.csv > bing.csv
-
-#Question 2
-cat nrc_*.csv > nrc.csv
-
-#Question 3
-cat wordcloud_y_*.csv > wordcloud_y.csv
-cat wordcloud_n_*.csv > wordcloud_n.csv
-R script -e "yes = read.csv('wordcloud_y.csv',header=T); \
-             no = read.csv('wordcloud_n.csv',header=T); \
-             yes = aggregate(yes$n, list(yes$word), FUN=sum); \
-             no = aggregate(no$n, list(no$word), FUN=sum); \
-             write.csv(yes, 'wordcloud_y.csv', col.names = TRUE); \
-             write.csv(no, 'wordcloud_n.csv', col.names = TRUE);"
+rm log/*.log
+rm error/*.err
+rm output/*.out
 
 # run your script
 Rscript question.R bing.csv nrc.csv wordcloud_y.csv wordcloud_n.csv
